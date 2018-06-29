6761907f0c8961e766b31c0b0a0cd68185b5f83b
Harry Gantz <harryhgantz@gmail.com>
2018-06-29 17:27:05 -0500

    added a few scripts to the prototype trying to figure out a good way to get this done

---
+++ Cards/Assets/Scripts/MovingObject.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovingObject : MonoBehaviour
+{
+
+	public float moveTime = 0.1f;
+	public LayerMask blockingLyer; //Determines if a space can be moved into
+
+	private BoxCollider _boxCollider;
+	private Rigidbody _rigidbody;
+	private float _inverseMoveTime;
+	
+	
+	protected virtual void Start ()
+	{
+		_boxCollider = GetComponent<BoxCollider>();
+		_rigidbody = GetComponent<Rigidbody>();
+		_inverseMoveTime = 1f / moveTime; // We can multiply instead of dividing which is more efficient computationally
+	}
+
+	protected bool Move (int xDir, int yDir, out RaycastHit2D hit)
+	{
+		Vector2 start = transform.position;
+		Vector2 end = start + new Vector2(xDir, yDir);
+
+		_boxCollider.enabled = false;
+		hit = Physics2D.Linecast(start, end, blockingLyer);
+		_boxCollider.enabled = true;
+
+		if (hit.transform == null)
+		{
+			StartCoroutine(SmoothMovement(end));
+			return true;
+		}
+		return false;
+	}
+	
+	//This moves units from one space to the next taking in a parameter end to specify where to move to
+	protected IEnumerator SmoothMovement(Vector3 end)
+	{
+		float sqrRemainingDistance = (transform.position - end).sqrMagnitude; //Using square magnitude because it is computationally cheaper than magnitude
+
+		while (sqrRemainingDistance > float.Epsilon)
+		{
+			//moves in a straight line to a target point
+			Vector3 newPosition = Vector3.MoveTowards(_rigidbody.position, end, _inverseMoveTime = Time.deltaTime);
+			_rigidbody.MovePosition(newPosition);
+			sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+			yield return null;
+		}
+	}
+
+	protected virtual void AttemptMove<T>(int xDir, int yDir)
+		where T : Component
+	{
+		RaycastHit2D hit;
+		bool canMove = Move(xDir, yDir, out hit);
+
+		if (hit.transform == null)
+			return;
+
+		T hitComponent = hit.transform.GetComponent<T>();
+		
+		if (!canMove && hitComponent != null)
+			OnCantMove((hitComponent));
+	}
+	protected abstract void OnCantMove<T>(T component)
+		where T : Component;
+}
---
+++ Cards/Assets/Scripts/Tutorial.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    private GameObject[] Deck;
+    private int currentDeckIndex;
+    private Vector2 touchOrigin = -Vector2.one; //initalizes the touch to off screen (i.e. false).
+    
+    private int horizontal;
+    private int vertical;
+
+
+    private void Start()
+    {
+        //By doing this the position of the cards in the deck matter as they will be chosen based on their position
+        //in the array.
+        Deck = new GameObject[transform.childCount];
+        
+        //Populate the array with chosen card types. In the tutorial we just have two cards.
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Deck[i] = transform.GetChild(i).gameObject;
+        }
+
+        currentDeckIndex = 0;
+    }
+
+    private void Update()
+    {
+
+        #region Player Input  
+#if UNITY_STANDALONE || UNITY_WEBGL || UNITY_EDITOR
+        horizontal = (int) (Input.GetAxisRaw("Horizontal"));
+        vertical = (int) (Input.GetAxisRaw("Vertical"));
+
+        if (horizontal != 0)
+        {
+            vertical = 0;
+        }
+#else
+        if (Input.touchCount > 0)
+        {
+            Touch myTouch = Input.touches[0];
+            
+            if (myTouch.phase == TouchPhase.Began)
+            {
+                touchOrigin = myTouch.position;
+            }
+            else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
+            {
+                Vector2 touchEnd = myTouch.position;
+                float x = touchEnd.x - touchOrigin.x;
+                float y = touchEnd.y - touchOrigin.y;
+                touchOrigin.x = -1;
+                if (Mathf.Abs(x) > Mathf.Abs(y))
+                    horizontal = x > 0 ? 1 : -1;
+                else
+                    vertical = y > 0 ? 1 : -1;
+            }
+        }
+#endif
+        #endregion
+        
+        
+    }
+}
