0ecb3ea5b87195a39c5982a2a7a19cf40286f2f6
Harry Gantz <harryhgantz@gmail.com>
2018-07-19 23:39:01 -0500

    Started working on scriptable object basic cards, added TextMesh Pro to project for more options when working with text in world space

---
+++ Cards/Assets/CardDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DEAL.ScriptableObjects;
+
+
+public class CardDisplay : MonoBehaviour
+{
+	public Card card;
+
+	public Text nameText;
+	public Text descriptionText;
+
+	public Material cardMaterial; 
+	void Start ()
+	{
+		nameText.text = card.name;
+		descriptionText.text = card.description;
+		cardMaterial = GetComponent<Material>();
+		cardMaterial = card.matertial;
+	}
+
+}
---
+++ Cards/Assets/Scripts/Card.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DEAL.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "New Base Card", menuName = "Cards/Base")]
+    public class Card : ScriptableObject
+    {
+        // Basic Info
+        public string name; // name of the card
+        public string description; // used to describe the card
+        public int pointValue; // the value the card is worth if swipped successfully
+        public Material matertial; //
+        
+
+    }
+}
+
