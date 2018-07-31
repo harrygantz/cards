419e80a3e9ba635b6aec4bbec02a06dd24c613d6
Harry Gantz <harryhgantz@gmail.com>
2018-07-30 21:55:39 -0500

    Working on finishing the object pooling for the cards

--- Cards/Assets/Scripts/Card.cs
+++ Cards/Assets/Scripts/Card.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 namespace DEAL.ScriptableObjects
 {
     [CreateAssetMenu(fileName = "New Base Card", menuName = "Cards/Base")]
-    public class Card : ScriptableObject
+    public class Card : ScriptableObject, IPooledObject
     {
         // Basic Info
-        public string name; // name of the card
+        public new string name; // name of the card
         public string description; // used to describe the card
         public int pointValue; // the value the card is worth if swipped successfully
         public Material matertial; //
         public Color32 color;
 
-
+        //Use this for initialization instead of Unity's Start() method
+        public void OnObjectSpawn()
+        {
+            
+        }
     }
 }
 
